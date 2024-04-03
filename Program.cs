52dc75d372f7c513e286b6a639a10b4019596154
User@ETYCZNY <biurohetmana@gmail.com>
2024-04-03 19:44:30 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Tyle mi zajelo asz 9 dni bo duzo jezdzilem po swiecie i teraz juz na troche pozostane przy nauce, Amen!");
+    }
+}
